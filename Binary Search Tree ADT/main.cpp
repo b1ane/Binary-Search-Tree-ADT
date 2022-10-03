64ab8ddfaa30e7bd0d59116be87fc460405a436e
Blane Zewdie <b1ane@outlook.com>
2022-10-03 13:57:04 -0500

    Initial Commit

---
+++ Binary Search Tree ADT/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Binary Search Tree ADT
+//
+//  Created by blane on 10/3/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
